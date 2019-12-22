51f439fa598e3af1f22e5d50b9777fa5e871fefd
Muco <mucahidkaraagac@gmail.com>
2019-12-23 01:33:22 +0300

    some methods of board class implementation

--- src/Connect4/Board.java
+++ src/Connect4/Board.java
@@ -43,5 +43,122 @@ public class Board {
         }
     }
 
+    public Board(String configuration) {
+        this();
+        for (int i = 0; i < configuration.length(); i++) {
+            int column = Integer.parseInt(configuration.charAt(i) + "");
+            if (hasWinningCondition() == false)
+                insertChipAt(column);
+        }
+    }
 
+    public void insertChipAt(int column) {
+        boolean inserted = false;
+        if (column < 1 || column > NUM_COLUMNS) {
+            throw new IllegalArgumentException("Enter valid Column ");
+        } else if (hasWinningCondition() == false) {
+            updateBoard(column);
+            inserted = true;
+            if (inserted == false)
+                throw new RuntimeException("Column is full");
+        } else {
+            throw new NullPointerException();
+        }
+    }
+
+    public void updateBoard(int column) {
+        if (totalMovesPlayed % 2 == 0) {
+            chips[findEmptyRow(column)][column - 1] = Chip.RED;
+            values[findEmptyRow(column)][column - 1] = 1;
+            totalMovesPlayed++;
+            config += column;
+        } else {
+            chips[findEmptyRow(column)][column - 1] = Chip.YELLOW;
+            values[findEmptyRow(column)][column - 1] = 2;
+            totalMovesPlayed++;
+            config += column;
+        }
+    }
+
+    public int findEmptyRow(int column) {
+        for (int i = NUM_ROWS - 1; i >= 0; i--) {
+            if (values[i][column - 1] == 0)
+                return i;
+        }
+        return -1;
+    }
+
+    private boolean hasWinningConditionOnColumns() {
+        for (int j = 0; j < NUM_COLUMNS; j++) {
+            for (int i = 0; i < NUM_ROWS / 2; i++) {
+                if (getChip(values, i, j) != Chip.NONE) {
+                    if (getChip(values, i, j) == getChip(values, i + 1, j)
+                            && getChip(values, i, j) == getChip(values, i + 2, j)
+                            && getChip(values, i, j) == getChip(values, i + 3, j)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean hasWinningConditionOnRows() {
+        for (int i = 0; i < NUM_ROWS; i++) {
+            for (int j = 0; j <= NUM_COLUMNS / 2; j++) {
+                if (getChip(values, i, j) != Chip.NONE) {
+                    if (getChip(values, i, j) == getChip(values, i, j + 1)
+                            && getChip(values, i, j) == getChip(values, i, j + 2)
+                            && getChip(values, i, j) == getChip(values, i, j + 3)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean hasWinningConditionOnDiagonals() {
+        // to right diagonal
+        for (int i = NUM_ROWS / 2; i < NUM_ROWS; i++) {
+            for (int j = 0; j <= NUM_COLUMNS / 2; j++) {
+                if (getChip(values, i, j) != Chip.NONE) {
+                    if (getChip(values, i, j) == getChip(values, i - 1, j + 1)
+                            && getChip(values, i, j) == getChip(values, i - 2, j + 2)
+                            && getChip(values, i, j) == getChip(values, i - 3, j + 3)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        // to left diagonal
+        for (int i = NUM_ROWS / 2; i < NUM_ROWS; i++) {
+            for (int j = NUM_COLUMNS / 2; j < NUM_COLUMNS; j++) {
+                if (getChip(values, i, j) != Chip.NONE) {
+                    if (getChip(values, i, j) == getChip(values, i - 1, j - 1)
+                            && getChip(values, i, j) == getChip(values, i - 2, j - 2)
+                            && getChip(values, i, j) == getChip(values, i - 3, j - 3)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean hasWinningCondition() {
+        return hasWinningConditionOnRows() || hasWinningConditionOnColumns() || hasWinningConditionOnDiagonals();
+    }
+
+    public Chip getChip(int[][] values, int row, int column) {
+        if (values[row][column] == 0) {
+            return Chip.NONE;
+        } else if (values[row][column] == 1) {
+            return Chip.RED;
+        } else if (values[row][column] == 2) {
+            return Chip.YELLOW;
+        }
+        return null;
+    }
 }
